d87424576447e21c18820623072c527dc4643ca3
haldokan <haldokan@gmail.com>
2015-08-06 23:38:18 -0400

    My sln to a Google interview question

--- JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/google/BinaryTreeIteratorWithLinkedBlockingQu.java
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/google/BinaryTreeIteratorWithLinkedBlockingQu.java
@@ -7,14 +7,15 @@ import java.util.concurrent.LinkedBlockingDeque;
 /**
  * My solution to a Google interview question.
  * 
- * TODO: solve the problem using SynchronousQueue instead of LinkedBlockingQueue
+ * Examine the other more robust solution I provide for this problem in BinaryTreeIteratorWithSynchQu.java.
  * 
  * Given a binary tree, implement an iterator that will iterate through its elements.
  * 
  * @author haldokan
  *
  */
-public class BinaryTreeIterator<E> implements Iterable<BinaryTreeIterator.Node<E>> {
+public class BinaryTreeIteratorWithLinkedBlockingQu<E> implements
+	Iterable<BinaryTreeIteratorWithLinkedBlockingQu.Node<E>> {
     private Node<E> root;
 
     public static void main(String[] args) {
@@ -32,14 +33,14 @@ public class BinaryTreeIterator<E> implements Iterable<BinaryTreeIterator.Node<E
 	n3.right = n6;
 	n6.left = n7;
 
-	BinaryTreeIterator<Integer> driver = new BinaryTreeIterator<>(root);
+	BinaryTreeIteratorWithLinkedBlockingQu<Integer> driver = new BinaryTreeIteratorWithLinkedBlockingQu<>(root);
 	for (Iterator<Node<Integer>> it = driver.iterator(); it.hasNext();) {
 	    Node<Integer> node = it.next();
 	    System.out.println(node);
 	}
     }
 
-    public BinaryTreeIterator(Node<E> root) {
+    public BinaryTreeIteratorWithLinkedBlockingQu(Node<E> root) {
 	this.root = root;
     }
 
@@ -52,13 +53,14 @@ public class BinaryTreeIterator<E> implements Iterable<BinaryTreeIterator.Node<E
     }
 
     @Override
-    public Iterator<BinaryTreeIterator.Node<E>> iterator() {
+    public Iterator<BinaryTreeIteratorWithLinkedBlockingQu.Node<E>> iterator() {
 	// I think it can be done using SychronousQueue but it requires more work
 	// BlockingQueue<Node<E>> iteratorStep = new SynchronousQueue<>();
 	BlockingQueue<Node<E>> iteratorQu = new LinkedBlockingDeque<>();
 	// start a new thread so we can be traversing the tree and iterating at the same time: more space and time
 	// efficient. Note that there is a flaw in this sln when iteration is faster than the tree traversal:
-	// iterator may find the queue empty while the traversing thread is trying to get the next node. I think this can
+	// iterator may find the queue empty while the traversing thread is trying to get the next node. I think this
+	// can
 	// happen in a large or deep tree; hence my original attempt was to use a SychronousQueue. Of course the other
 	// solution is to start the traversal in the same thread calling the iterator but that is less efficient time &
 	// space: time because it is sequential and space because it adds treeSize * 64 bits on a 64-bit JVM.
@@ -71,7 +73,7 @@ public class BinaryTreeIterator<E> implements Iterable<BinaryTreeIterator.Node<E
 	return new IteratorInternal<E>(iteratorQu);
     }
 
-    private static class IteratorInternal<E> implements Iterator<BinaryTreeIterator.Node<E>> {
+    private static class IteratorInternal<E> implements Iterator<BinaryTreeIteratorWithLinkedBlockingQu.Node<E>> {
 	private final BlockingQueue<Node<E>> iteratorQu;
 
 	public IteratorInternal(BlockingQueue<Node<E>> iteratorQu) {
@@ -107,5 +109,4 @@ public class BinaryTreeIterator<E> implements Iterable<BinaryTreeIterator.Node<E
 	    return "(" + value + ")";
 	}
     }
-
 }
---
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/google/BinaryTreeIteratorWithSynchQu.java
@@ -0,0 +1,128 @@
+package org.haldokan.edge.interviewquest.google;
+
+import java.util.Iterator;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * My solution to a Google interview question.
+ * 
+ * Given a binary tree, implement an iterator that will iterate through its elements.
+ * 
+ * @author haldokan
+ *
+ */
+public class BinaryTreeIteratorWithSynchQu<E> implements Iterable<BinaryTreeIteratorWithSynchQu.Node<E>> {
+    private Node<E> root;
+
+    public static void main(String[] args) {
+	Node<Integer> root = new Node<>(7);
+	Node<Integer> n2 = new Node<>(4);
+	Node<Integer> n3 = new Node<>(9);
+	Node<Integer> n4 = new Node<>(6);
+	Node<Integer> n5 = new Node<>(1);
+	Node<Integer> n6 = new Node<>(12);
+	Node<Integer> n7 = new Node<>(10);
+	root.left = n2;
+	root.right = n3;
+	n2.right = n4;
+	n2.left = n5;
+	n3.right = n6;
+	n6.left = n7;
+
+	BinaryTreeIteratorWithSynchQu<Integer> driver = new BinaryTreeIteratorWithSynchQu<>(root);
+	for (Iterator<Node<Integer>> it = driver.iterator(); it.hasNext();) {
+	    Node<Integer> node = it.next();
+	    System.out.println(node);
+	}
+	System.out.println("Calling hasNext multiple times should have no effect on advancing the iterator");
+	for (Iterator<Node<Integer>> it = driver.iterator(); it.hasNext();) {
+	    for (int i = 0; i < 5; i++)
+		it.hasNext();
+	    Node<Integer> node = it.next();
+	    System.out.println(node);
+	}
+    }
+
+    public BinaryTreeIteratorWithSynchQu(Node<E> root) {
+	this.root = root;
+    }
+
+    private void inorder(Node<E> tree, BlockingQueue<Node<E>> iteratorQu) {
+	if (tree == null)
+	    return;
+	inorder(tree.left, iteratorQu);
+	for (;;) {
+	    try {
+		// 'put' on SynchronousQueue waits for consumer threads to become available (unlike 'offer')
+		iteratorQu.put(tree);
+		break;
+	    } catch (InterruptedException e) {
+		// ignore and go back to blocking on queue
+	    }
+	}
+	inorder(tree.right, iteratorQu);
+    }
+
+    @Override
+    public Iterator<BinaryTreeIteratorWithSynchQu.Node<E>> iterator() {
+	BlockingQueue<Node<E>> iteratorQu = new SynchronousQueue<>();
+	new Thread(new Runnable() {
+	    @Override
+	    public void run() {
+		inorder(root, iteratorQu);
+	    }
+	}).start();
+	return new IteratorInternal<E>(iteratorQu);
+    }
+
+    private static class IteratorInternal<E> implements Iterator<BinaryTreeIteratorWithSynchQu.Node<E>> {
+	private final BlockingQueue<Node<E>> iteratorQu;
+	private Node<E> nextNode;
+
+	public IteratorInternal(BlockingQueue<Node<E>> iteratorQu) {
+	    this.iteratorQu = iteratorQu;
+	}
+
+	@Override
+	public boolean hasNext() {
+	    if (nextNode == null) {
+		for (;;) {
+		    try {
+			// the timeout is needed to give the thread the traverse the tree to start and put nodes on the
+			// synch queue
+			nextNode = iteratorQu.poll(10, TimeUnit.MILLISECONDS);
+			break;
+		    } catch (InterruptedException e) {
+			// ignore and go back to blocking on the queue
+		    }
+		}
+		return nextNode != null;
+	    }
+	    return true;
+	}
+
+	@Override
+	public Node<E> next() {
+	    Node<E> next = nextNode;
+	    nextNode = null;
+	    return next;
+	}
+    }
+
+    public static class Node<E> {
+	private final E value;
+	private Node<E> left;
+	private Node<E> right;
+
+	public Node(E v) {
+	    this.value = v;
+	}
+
+	@Override
+	public String toString() {
+	    return "(" + value + ")";
+	}
+    }
+}
