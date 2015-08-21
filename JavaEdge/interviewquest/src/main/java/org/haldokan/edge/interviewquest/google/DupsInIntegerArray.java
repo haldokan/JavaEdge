bb0f3e11ce1965c104183d0ff7b2b7cffb2c7f7f
haldokan <haldokan@gmail.com>
2015-08-20 21:43:11 -0400

    Google interview questions pending solutions

---
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/google/DupsInIntegerArray.java
@@ -0,0 +1,17 @@
+package org.haldokan.edge.interviewquest.google;
+
+/**
+ * My solution to a Google interview question
+ * 
+ * 
+ * Find the duplicate in an array of length n. The values are positive integers in the range between 1 .. n-1.
+ * 
+ * The way I understand the is question is that all numbers in the range must be present. Since the range in 1 to n -1 there will
+ * always be an exactly one dup to fill the array of length n.
+ * 
+ * @author haldokan
+ *
+ */
+public class DupsInIntegerArray {
+
+}
---
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/google/GeoBasedHotelRecommendations.java
@@ -0,0 +1,16 @@
+package org.haldokan.edge.interviewquest.google;
+
+/**
+ * My solution to a Google interview question
+ * 
+ * Design an application which sends the user the 10 closest hotels based on his geo location.
+ * 
+ * User who posted this problem on Careercup said: When i asked roughly how many hotels in total are we talking about, I
+ * was asked to estimate how many hotels are there in the world...
+ * 
+ * @author haldokan
+ *
+ */
+public class GeoBasedHotelRecommendations {
+
+}
