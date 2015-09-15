a53f7b4b696d8e2aa2bc5c353d4a2f3017f5dd1c
haytham.aldokanji <haytham.aldokanji@lab49.com>
2015-09-15 12:28:57 -0400

    Learning Guice

---
+++ JavaEdge/lunch-box/src/main/java/org/haldokan/edge/lunchbox/guice/intro/DomainGraph.java
@@ -0,0 +1,13 @@
+package org.haldokan.edge.lunchbox.guice.intro;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by haytham.aldokanji on 9/15/15.
+ */
+public class DomainGraph {
+    List<Item> find(String query) {
+        return Collections.emptyList();
+    }
+}
---
+++ JavaEdge/lunch-box/src/main/java/org/haldokan/edge/lunchbox/guice/intro/Item.java
@@ -0,0 +1,7 @@
+package org.haldokan.edge.lunchbox.guice.intro;
+
+/**
+ * Created by haytham.aldokanji on 9/15/15.
+ */
+public class Item {
+}
