4cbb8020f96389d96e61bf92ecbaaee1aa0921f8
haytham.aldokanji <haytham.aldokanji@lab49.com>
2015-10-05 12:47:50 -0400

    My sln to a Google interview question

--- JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/google/AggregatingDataFromLargeLogFiles.java
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/google/AggregatingDataFromLargeLogFiles.java
@@ -1,11 +1,12 @@
 package org.haldokan.edge.interviewquest.google;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
- * I cannot think of any valid solution to this friggen problem. Put off for a better day
+ * My solution to a Google interview question. Complexity of the presented solution is O(nlogn) for sorting plus n for
+ * scanning which yields asymptotic complexity of O(nlogn). Space complexity is O(n).
  * <p>
  * You are given large numbers of logs, each one of which contains a start time (long), end time (long) and memory usage (int).
  * The time is recorded as MMDDHH (100317 means October 3rd 5PM) Write an algorithm that returns a specific time (hour)
@@ -15,83 +16,102 @@ import java.util.List;
  * 100207 100208 2
  * 100305 100307 5
  * 100207 100209 4
+ * 100209 100211 5
+ * 100208 100210 3
  * 111515 121212 1
- * Answer: 100207
+ * Answer: 100209 8
  * <p>
  * (Need to consider different scenarios like the time slots could be very sparse)
  * Created by haytham.aldokanji on 10/1/15.
  */
 public class AggregatingDataFromLargeLogFiles {
+    List<Measurement> measurements = new ArrayList<>();
 
-    public void addMeasure(long t1, long t2, int val) {
-        Measure m = new Measure(t1, t2, val);
-//        List<Measure> ms = m.partition()
+    public static void main(String[] args) {
+        AggregatingDataFromLargeLogFiles driver = new AggregatingDataFromLargeLogFiles();
+        driver.addMeasurement(Measurement.start(100209, 5));
+        driver.addMeasurement(Measurement.end(100211, 5));
+
+        driver.addMeasurement(Measurement.start(100305, 5));
+        driver.addMeasurement(Measurement.end(100307, 5));
+
+        driver.addMeasurement(Measurement.start(100207, 2));
+        driver.addMeasurement(Measurement.end(100208, 2));
+
+        driver.addMeasurement(Measurement.start(100207, 4));
+        driver.addMeasurement(Measurement.end(100209, 4));
+
+        driver.addMeasurement(Measurement.start(100208, 3));
+        driver.addMeasurement(Measurement.end(100210, 3));
+
+        driver.addMeasurement(Measurement.start(111515, 1));
+        driver.addMeasurement(Measurement.end(121212, 1));
+
+        System.out.println(driver.topMeasurement());
     }
 
-    private static class Measure {
-        private long t1, t2;
-        private int val;
+    public void addMeasurement(Measurement m) {
+        measurements.add(m);
+    }
 
-        public Measure(long t1, long t2, int val) {
-            this.t1 = t1;
-            this.t2 = t2;
-            this.val = val;
-        }
-
-        private boolean intersect(Measure m) {
-            return (t1 >= m.t1 && t1 <= m.t2 || t2 >= m.t1 && t2 <= m.t2);
-        }
-
-        private boolean partOf(long t1, long t2) {
-            return t1 >= this.t1 && t2 <= this.t2;
-        }
-
-        public List<Measure> partition(Measure m) {
-            List<Measure> partitions = new ArrayList<>();
-            if (intersect(m)) {
-                long[] bounds = new long[4];
-                bounds[0] = t1;
-                bounds[1] = t2;
-                bounds[2] = m.t1;
-                bounds[3] = m.t2;
-                Arrays.sort(bounds);
-                // both ranges are the same
-                if (bounds[0] == bounds[1] && bounds[2] == bounds[3]) {
-                    partitions.add(new Measure(t1, t2, val + m.val));
-                } else if (bounds[0] == bounds[1] && bounds[2] < bounds[3]) {
-                    partitions.add(new Measure(bounds[0], bounds[1], val + m.val));
-                    int pval = val;
-                    if (m.partOf(bounds[2], bounds[3]))
-                        pval = m.val;
-                    partitions.add(new Measure(bounds[2], bounds[3], pval));
-                } else if (bounds[2] == bounds[3] && bounds[1] > bounds[0]) {
-                    partitions.add(new Measure(bounds[2], bounds[3], val + m.val));
-                    int pval = val;
-                    if (m.partOf(bounds[0], bounds[1]))
-                        pval = m.val;
-                    partitions.add(new Measure(bounds[0], bounds[1], pval));
-                } else if (bounds[0] < bounds[1] && bounds[1] < bounds[2] && bounds[2] < bounds[3]) {
-                    int pval1 = val;
-                    int pval2 = m.val;
-                    if (m.partOf(bounds[0], bounds[1])) {
-                        pval1 = m.val;
-                        pval2 = val;
-                    }
-                    partitions.add(new Measure(bounds[0], bounds[1], pval1));
-                    partitions.add(new Measure(bounds[1], bounds[2], pval1 + pval2));
-                    partitions.add(new Measure(bounds[2], bounds[3], pval2));
+    public Measurement topMeasurement() {
+        // we cannot simply implement the comparator as m1.time - m2.time (have to cast long to int which I don't like).
+        Collections.sort(measurements);
+        System.out.println(measurements);
+        int top = 0;
+        long time = 0L;
+        int current = 0;
+        for (Measurement m : measurements) {
+            if (m.isStart()) {
+                current += m.val;
+                if (current > top) {
+                    top = current;
+                    time = m.time;
                 }
+            } else {
+                current -= m.val;
             }
-            return partitions;
+        }
+        return Measurement.start(time, top);
+    }
+
+    private static class Measurement implements Comparable<Measurement> {
+        private long time;
+        private int val;
+        private int start;
+
+        private Measurement(long time, int val, int start) {
+            this.time = time;
+            this.val = val;
+            this.start = start;
+        }
+
+        public static Measurement start(long time, int val) {
+            return new Measurement(time, val, 1);
+        }
+
+        public static Measurement end(long time, int val) {
+            return new Measurement(time, val, 0);
+        }
+
+        public boolean isStart() {
+            return start == 1;
         }
 
         @Override
         public String toString() {
-            return "Measure{" +
-                    "t1=" + t1 +
-                    ", t2=" + t2 +
+            return "Measurement{" +
+                    "time=" + time +
                     ", val=" + val +
+                    ", start=" + start +
                     '}';
+        }
+
+        @Override
+        public int compareTo(Measurement o) {
+            // if an interval starts at the same time that another interval ends we want to process the ending interval first.
+            // This has implication on the calculated top values
+            return time - o.time > 0 ? 1 : time - o.time < 0 ? -1 : start - o.start;
         }
     }
 }
