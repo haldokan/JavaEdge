310b8b581640d0d5e68c019525f3fb425252fc5e
haytham.aldokanji <haytham.aldokanji@lab49.com>
2016-04-08 16:42:00 -0400

    My solutions to Amazon interview question

--- JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/amazon/SumCombinationsInArray.java
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/amazon/SumCombinationsInArray.java
@@ -1,7 +1,13 @@
 package org.haldokan.edge.interviewquest.amazon;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * Given an integer array , find all pair combinations which sum to a given number. If a number is used once, it must not be used again.
+ * My solution to an Amazon interview question in O(n)
+ *
+ * Given an integer array, find all pair combinations which sum to a given number.
+ * If a number is used once, it must not be used again.
  * eg if input array is 6444 and sum =10
  * output must be just 6 4
  * Give an O(n) solution
@@ -10,7 +16,27 @@ package org.haldokan.edge.interviewquest.amazon;
 public class SumCombinationsInArray {
 
     public static void main(String[] args) {
+        pairCombs(new int[]{6, 4, 4, 3, 2, 7, 6, 8, 7, 3}, 10);
+        pairCombs(new int[]{-6, -4, -4, -3, -2, -7, -6, -8, -7, -3}, -10);
+    }
 
+    private static void pairCombs(int[] nums, int k) {
+        Map<Integer, Integer> complements = new HashMap<>();
+
+        for (int num : nums) {
+            if (complements.containsKey(num)) {
+                System.out.println(k - num + ", " + num);
+
+                int howmany = complements.get(num);
+                if (howmany == 1) {
+                    complements.remove(num);
+                } else {
+                    complements.put(num, howmany - 1);
+                }
+            } else {
+                complements.put(k - num, 1);
+            }
+        }
 
     }
 }
