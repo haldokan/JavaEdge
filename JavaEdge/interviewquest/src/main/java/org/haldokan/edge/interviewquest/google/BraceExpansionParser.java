c16a981d9a08dd86eb6c217592ee587a6afd062b
haytham.aldokanji <haytham.aldokanji@lab49.com>
2016-05-03 22:54:06 -0400

    My solution to a Google interview question

--- JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/google/BraceExpansionParser.java
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/google/BraceExpansionParser.java
@@ -1,9 +1,10 @@
 package org.haldokan.edge.interviewquest.google;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 /**
+ * TODO: provide a solution later?
+ * This is similar (though simpler) to the Facebook interview question that I provide a solution for here:
+ * EvaluateVarInLinearEquation.java
+ *
  * Write code that would parse an expression that is similar to BASH brace expansion. Best illustrated with an example:
  * the expression "(a,b,cy)n,m" would be parsed into an array of the following strings:
  * an
@@ -22,27 +23,4 @@ import java.util.Deque;
  * Created by haytham.aldokanji on 10/5/15.
  */
 public class BraceExpansionParser {
-    private Deque<Character> stack = new ArrayDeque<>();
-
-    public static void main(String[] args) {
-        BraceExpansionParser driver = new BraceExpansionParser();
-        driver.parse("((a,b)o(m,n)p,b)");
-    }
-
-    public void parse(String expr) {
-        for (int i = 0; i < expr.length(); i++) {
-            char curr = expr.charAt(i);
-            Character prev = stack.peek();
-            if (exprOperand(curr) && prev != null && prev.charValue() == ')') {
-
-            } else {
-                stack.push(curr);
-            }
-        }
-        System.out.println(stack);
-    }
-
-    private boolean exprOperand(char ch) {
-        return !(ch == ')' || ch == '(' || ch == ',');
-    }
 }
