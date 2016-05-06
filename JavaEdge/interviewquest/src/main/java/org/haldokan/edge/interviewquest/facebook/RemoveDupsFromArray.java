bbe6ad57024d907dd8decf778ee362044f5fc0ef
haytham.aldokanji <haytham.aldokanji@lab49.com>
2016-05-06 17:20:17 -0400

    My solution to a Facebook interview question

---
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/facebook/RemoveDupsFromArray.java
@@ -0,0 +1,34 @@
+package org.haldokan.edge.interviewquest.facebook;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * My solution to a Facebook interview question - silly question!
+ * <p>
+ * Code a function that receives an array with duplicates and returns a new array keeping the original order of
+ * the elements but with the duplicates removed.
+ * <p>
+ * <p>
+ * For example, if the input were
+ *
+ * @[ @"dog", @"cat", @"dog", @"fish" ]
+ * the output would be
+ * @[ @"dog", @"cat", @"fish" ]
+ * Tell the complexity of the solution.
+ * Created by haytham.aldokanji on 5/6/16.
+ */
+public class RemoveDupsFromArray {
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[]{1, 2, 3, 4, 2, 2, 3, 5, 3, 6, 1, 7};
+        Integer[] unique = removeDups(arr);
+        System.out.println(Arrays.toString(unique));
+    }
+
+    public static Integer[] removeDups(Integer[] arr) {
+        Set<Integer> unique = new LinkedHashSet<>();
+        Arrays.stream(arr).forEach(unique::add);
+        return unique.stream().toArray(Integer[]::new);
+    }
+}
