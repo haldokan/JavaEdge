a1ce9bd1f4dc0e382db901b200ae102dd93523df
haytham.aldokanji <haytham.aldokanji@lab49.com>
2016-05-09 15:47:20 -0400

    My solution to a Facebook interview question

---
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/facebook/MergingSortedArraysRmvDups.java
@@ -0,0 +1,65 @@
+package org.haldokan.edge.interviewquest.facebook;
+
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * My solution to a Facebook interview question
+ * <p>
+ * Given two arrays of sorted integers, merge them keeping in mind that there might be common elements in the arrays
+ * and that common elements must only appear once in the merged array.
+ * <p>
+ * Created by haytham.aldokanji on 5/8/16.
+ */
+public class MergingSortedArraysRmvDups {
+
+    public static void main(String[] args) {
+        MergingSortedArraysRmvDups driver = new MergingSortedArraysRmvDups();
+        driver.test();
+    }
+
+    public int[] merge(int[] arr1, int[] arr2) {
+        int[][] arrs = new int[][]{arr1, arr2};
+        Arrays.sort(arrs, (a1, a2) -> a2.length - a1.length);
+
+        int[] first = arrs[0];
+        int[] second = arrs[1];
+
+        int[] merged = new int[arr1.length + arr2.length];
+        int secondIndex = 0;
+        int mergeIndex = 0;
+
+        for (int val : first) {
+            while (secondIndex < second.length && val > second[secondIndex]) {
+                int prevMergeIndex = mergeIndex - 1;
+                if (prevMergeIndex < 0 || merged[prevMergeIndex] != second[secondIndex]) {
+                    merged[mergeIndex++] = second[secondIndex];
+                }
+                secondIndex++;
+            }
+            int prevMergeIndex = mergeIndex - 1;
+            if (prevMergeIndex < 0 || merged[prevMergeIndex] != val) {
+                merged[mergeIndex++] = val;
+            }
+        }
+        return Arrays.copyOf(merged, mergeIndex);
+    }
+
+    private void test() {
+        int[] arr1 = new int[]{4, 4, 6, 8, 8, 10, 14, 14};
+        int[] arr2 = new int[]{1, 1, 2, 3, 5, 5, 6, 7, 9, 11, 12, 13, 15, 15};
+
+        int[] merged = merge(arr1, arr2);
+        assertThat(merged, is(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}));
+
+        arr1 = new int[]{4, 4, 4, 5, 5, 5, 6, 7};
+        arr2 = new int[]{1, 1, 3, 3, 6, 7};
+
+        merged = merge(arr1, arr2);
+        assertThat(merged, is(new int[]{1, 3, 4, 5, 6, 7}));
+
+    }
+
+}
