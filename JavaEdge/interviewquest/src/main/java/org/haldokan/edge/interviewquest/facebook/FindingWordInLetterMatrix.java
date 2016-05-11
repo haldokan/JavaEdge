26136336ab5f383d7d5efe2b097cfb1da7b802b8
haytham.aldokanji <haytham.aldokanji@lab49.com>
2016-05-11 00:18:33 -0400

    My solution to a Facebook interview question

--- JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/facebook/FindingWordInLetterMatrix.java
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/facebook/FindingWordInLetterMatrix.java
@@ -216,39 +216,6 @@ public class FindingWordInLetterMatrix {
         assertThat(path.isPresent(), is(false));
     }
 
-    private static class Cell {
-        private final int row, column;
-
-        public Cell(int row, int column) {
-            this.row = row;
-            this.column = column;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            Cell cell = (Cell) o;
-
-            if (row != cell.row) return false;
-            return column == cell.column;
-
-        }
-
-        @Override
-        public int hashCode() {
-            int result = row;
-            result = 31 * result + column;
-            return result;
-        }
-
-        @Override
-        public String toString() {
-            return "(" + row + ", " + column + ")";
-        }
-    }
-
     private static class MatrixCell {
         private final int row, column;
 
