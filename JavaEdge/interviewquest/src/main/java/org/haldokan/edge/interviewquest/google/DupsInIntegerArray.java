2055682332a2d62b16f8bdfe7e6deca00199ad8e
haytham.aldokanji <haytham.aldokanji@lab49.com>
2016-05-19 21:33:50 -0400

    My solution to a Google interview question

--- JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/google/DupsInIntegerArray.java
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/google/DupsInIntegerArray.java
@@ -1,7 +1,10 @@
 package org.haldokan.edge.interviewquest.google;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
- * My solution to a Google interview question
+ * My solution to a Google interview question - in situ with O(n) time
  * <p>
  * <p>
  * Find the duplicate in an array of length n. The values are positive integers in the range between 1 .. n-1.
@@ -13,4 +16,31 @@ package org.haldokan.edge.interviewquest.google;
  */
 public class DupsInIntegerArray {
 
+    public static void main(String[] args) {
+        DupsInIntegerArray driver = new DupsInIntegerArray();
+
+        int[] arr = new int[]{4, 3, 1, 2, 3};
+        assertThat(driver.duplicate(arr), is(3));
+
+        arr = new int[]{4, 3, 1, 2, 9, 5, 8, 6, 7, 9};
+        assertThat(driver.duplicate(arr), is(9));
+
+        arr = new int[]{1, 4, 3, 1, 2, 9, 5, 8, 6, 7};
+        assertThat(driver.duplicate(arr), is(1));
+    }
+
+    public int duplicate(int[] arr) {
+        int dup = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int valAtIndex = arr[arr[0]];
+
+            if (arr[0] == valAtIndex) {
+                dup = arr[0];
+            }
+            int tmp = arr[0];
+            arr[0] = valAtIndex;
+            arr[tmp] = tmp;
+        }
+        return dup;
+    }
 }
