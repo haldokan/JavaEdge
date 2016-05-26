40c39bc94c8041c037ab27f599dceff4b053cbf1
haytham.aldokanji <haytham.aldokanji@lab49.com>
2016-05-26 17:30:51 -0400

    My solution to a Google interview question

--- JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/google/TaskScheduler.java
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/google/TaskScheduler.java
@@ -1,5 +1,6 @@
 package org.haldokan.edge.interviewquest.google;
 
+import com.google.common.base.Stopwatch;
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
 import com.google.common.eventbus.EventBus;
@@ -8,23 +9,31 @@ import com.google.common.eventbus.Subscribe;
 import java.time.LocalTime;
 import java.util.*;
 import java.util.concurrent.*;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 /**
- * My solution to a Google interview question - @Inprogress
- *
- * Given the interface below, implement a task scheduler. As a topper support parallel task execution
+ * My solution to a Google interview question - I supported concurrency on two levels: the job level, in that the tasks
+ * in one job submission run concurrently with other job submissions. The 2nd level is within the same job: in the same
+ * job submission tasks that are not dependent on each other run concurrently. Dependent tasks run only after the successful
+ * completion of their dependencies: I achieved that with a DAG DFS that sorted the tasks topographically. When a task fails
+ * all its dependent tasks are not run and are marked as failed. Re-submission of a failed job runs only the failed tasks.
+ * Tasks that complete successfully cannot be rerun because the task scheduler keeps a record of task run statuses. Task
+ * run statuses are communicated to other running tasks via an event bus (Guava EventBus). Every task runs in its own
+ * pooled thread and it is 'awaited' on a latch that is counted down every time a dependency of the task completes successfully.
+ * No synchronization constructs were used in this solution apart from the R/W lock built into to the Java ConcurrentHashMap.
  * <p>
+ * <p>
+ * Given the interface below, implement a task scheduler that concurrently runs tasks
  * interface Task {
  * void Run();
  * Set<Task> GetDependencies();
  * }
- * Additionally, the task scheduler should follow two rules.
- * <p>
+ * Additionally, the task scheduler should follow two rules:
  * 1. Each task may only be executed once.
  * 2. The dependencies of a task should be executed before the task itself.
  * <p>
@@ -46,7 +55,11 @@ public class TaskScheduler {
         Tester tester = scheduler.new Tester();
 
         tester.testDependencyTopoSort();
-        tester.testRunTask();
+        tester.testGoodRun();
+        tester.testTaskFailurePropagation();
+        tester.testFailedTaskRerun();
+
+        scheduler.taskRunner.shutdown();
     }
 
     public static TaskScheduler create() {
@@ -68,7 +81,7 @@ public class TaskScheduler {
     private enum TaskRunState {PENDING, RUNNING, COMPLETED, IN_ERROR}
 
     private static class DependencyGraph {
-        // edge is not important for our problem so we simply go with a Boolean
+        // edge is not important for our solution so we simply go with a Boolean
         private Table<Task, Task, Boolean> graph = HashBasedTable.create();
 
         public static DependencyGraph create() {
@@ -144,7 +157,8 @@ public class TaskScheduler {
                     tasksToRun.add(t);
                 }
             });
-            Arrays.stream(tasks).forEach(taskRunner::submit);
+            tasksToRun.forEach(Task::registerWithEventBus);
+            tasksToRun.forEach(taskRunner::submit);
             return Optional.of(tasksToRun.toArray(new Task[tasksToRun.size()]));
         }
     }
@@ -153,68 +167,73 @@ public class TaskScheduler {
         private final String id;
         private final LocalTime time;
         private final Set<Task> dependencies;
-        private volatile boolean abort;
+        private volatile boolean aborted;
         private CountDownLatch latch;
 
-        private Task(String id, LocalTime time) {
+        private Task(String id, LocalTime time, Task... dependencies) {
             this.id = id;
             this.time = time;
-            this.dependencies = new HashSet<>();
+            this.dependencies = Arrays.stream(dependencies).collect(Collectors.toSet());
         }
 
-        public void addDependencies(Task... dependencies) {
-            Arrays.stream(dependencies).forEach(this.dependencies::add);
+        public void registerWithEventBus() {
+            latch = new CountDownLatch((int) this.dependencies.stream()
+                    .filter(d -> TaskRunState.PENDING == taskRunStateMap.get(d.id))
+                    .count());
+            taskEventBus.register(this);
         }
 
         public void run() {
-            if (taskRunStateMap.get(id) != TaskRunState.PENDING) {
-                System.out.println("Skipping running task '"
-                        + id + "' because it is in state '" + taskRunStateMap.get(id) + "'");
-                taskEventBus.post(new RunStateEvent(id, TaskRunState.COMPLETED));
-                return;
-            }
-            latch = new CountDownLatch(dependencies.size());
             for (; ; ) {
                 try {
                     latch.await();
-                    if (abort) {
-                        System.out.println("@" + id + ": abort");
-                        taskRunStateMap.put(id, TaskRunState.IN_ERROR);
-                        taskEventBus.post(new RunStateEvent(id, TaskRunState.IN_ERROR));
-                        return;
+                    if (aborted) {
+                        System.out.printf("@%s->aborted%n", id);
+                        updateState(TaskRunState.IN_ERROR);
                     }
                     break;
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-            taskRunStateMap.put(id, TaskRunState.RUNNING);
-            System.out.print(id);
-
-            IntStream.range(0, 5).forEach(i -> {
-                System.out.print("..." + taskRunStateMap.get(id).toString().toLowerCase());
+            //once started to run or aborted we are not interested in any event notifications
+            taskEventBus.unregister(this);
+            if (aborted) {
+                return;
+            }
+            updateState(TaskRunState.RUNNING);
+            System.out.printf("@%s", id);
+            for (int i = 0; i < 5; i++) {
+                System.out.printf("...%s", taskRunStateMap.get(id));
                 try {
                     Thread.sleep(100);
+                    if (time == null) {
+                        throw new RuntimeException("Simulated failure for task: " + id);
+                    }
                 } catch (Exception e) {
-                    taskRunStateMap.put(id, TaskRunState.IN_ERROR);
-                    taskEventBus.post(new RunStateEvent(id, TaskRunState.IN_ERROR));
-                    e.printStackTrace();
+                    updateState(TaskRunState.IN_ERROR);
+                    System.out.printf("%s%n", e);
+                    // this exception shows only when the Future.get is called
+                    throw new RuntimeException(e);
                 }
-            });
-            taskRunStateMap.put(id, TaskRunState.COMPLETED);
-            taskEventBus.post(new RunStateEvent(id, TaskRunState.COMPLETED));
-            System.out.println("-->" + taskRunStateMap.get(id).toString().toLowerCase());
+            }
+            System.out.printf("%n");
+            updateState(TaskRunState.COMPLETED);
         }
 
         @Subscribe
         public void onEvent(RunStateEvent event) {
+            // not interested in the RUNNING state including for this task dependencies
+            if (event.state == TaskRunState.RUNNING) {
+                return;
+            }
             boolean isDependency = dependencies.stream().map(d -> d.id).anyMatch(id -> id.equals(event.taskId));
             if (isDependency) {
-                System.out.println("@" + id + ": " + event);
+                System.out.printf("@%s-sub->%s%n", id, event);
                 if (event.state == TaskRunState.IN_ERROR) {
-                    abort = true;
+                    aborted = true;
                     LongStream.range(0l, latch.getCount()).forEach(i -> latch.countDown());
-                } else {
+                } else if (event.state == TaskRunState.COMPLETED) {
                     latch.countDown();
                 }
             }
@@ -230,9 +249,7 @@ public class TaskScheduler {
             if (o == null || getClass() != o.getClass()) return false;
 
             Task task = (Task) o;
-
             return !(id != null ? !id.equals(task.id) : task.id != null);
-
         }
 
         @Override
@@ -243,6 +260,12 @@ public class TaskScheduler {
         @Override
         public String toString() {
             return id;
+        }
+
+        private void updateState(TaskRunState state) {
+            System.out.printf("@%s-pub->%s%n", id, state);
+            taskRunStateMap.put(id, state);
+            taskEventBus.post(new RunStateEvent(id, state));
         }
     }
 
@@ -257,36 +280,26 @@ public class TaskScheduler {
 
         @Override
         public String toString() {
-            return taskId + "->" + state;
+            return taskId + ":" + state;
         }
     }
 
     private class Tester {
         private Task task1, task2, task3, task4, task5, task6;
 
-        private void makeTasks() {
-            task1 = new Task("T1", LocalTime.now());
-            task2 = new Task("T2", LocalTime.now().minusHours(1));
-            task3 = new Task("T3", LocalTime.now().minusHours(2));
-            task4 = new Task("T4", LocalTime.now().minusHours(3));
-            task5 = new Task("T5", LocalTime.now().minusHours(4));
+        private void makeTasks(boolean addFailingTask) {
+            //mind the order!
             task6 = new Task("T6", LocalTime.now().minusHours(5));
-
-        }
-
-        private void makeDependencies() {
-            task1.addDependencies(task2, task3);
-            task2.addDependencies(task3, task4);
-            task3.addDependencies(task4, task5, task6);
-            task4.addDependencies(task5, task6);
-            task5.addDependencies(task6);
+            task5 = new Task("T5", LocalTime.now().minusHours(4), task6);
+            LocalTime task4Time = addFailingTask ? null : LocalTime.now().minusHours(3);
+            task4 = new Task("T4", task4Time, task5, task6);
+            task3 = new Task("T3", LocalTime.now().minusHours(2), task4, task5, task6);
+            task2 = new Task("T2", LocalTime.now().minusHours(1), task3, task4);
+            task1 = new Task("T1", LocalTime.now(), task2);
         }
 
         private DependencyGraph makeDependencyGraph() {
-            makeTasks();
-            makeDependencies();
             DependencyGraph dependencyGraph = DependencyGraph.create();
-
             dependencyGraph.add(task1, task2);
             dependencyGraph.add(task2, task3);
             dependencyGraph.add(task2, task4);
@@ -300,32 +313,96 @@ public class TaskScheduler {
             return dependencyGraph;
         }
 
-        public void testDependencyTopoSort() {
+        private void pollAndAssertTaskRunState(Task[] tasks, TaskRunState[] states) throws InterruptedException {
+            Stopwatch watch = Stopwatch.createStarted();
+            for (; ; ) {
+                Thread.sleep(50);
+                boolean done = true;
+                for (int i = 0; i < tasks.length; i++) {
+                    done &= states[i] == taskRunStateMap.get(tasks[i].id);
+                }
+                if (done) {
+                    break;
+                }
+                if (watch.elapsed(TimeUnit.SECONDS) > 5) {
+                    fail("timed out waiting: "
+                            + Arrays.toString(tasks) + "\n"
+                            + Arrays.toString(states) + "\n"
+                            + taskRunStateMap);
+                }
+            }
+        }
+
+        private void testDependencyTopoSort() {
+            makeTasks(false);
             DependencyGraph dependencyGraph = makeDependencyGraph();
             Task[] dependencies = dependencyGraph.sortTopographically(task1);
-            // how tasks 5 and 6 are listed depends on how the dfs traverses the graph
             assertThat(dependencies, is(new Task[]{task6, task5, task4, task3, task2, task1}));
         }
 
-        private void testRunTask() throws Exception {
-            makeTasks();
-            makeDependencies();
+        private void testGoodRun() throws Exception {
+            taskRunStateMap.clear();
+            makeTasks(false);
 
             Task[] expected = new Task[]{task6, task5, task4, task3, task2, task1};
-
+            TaskRunState[] states = new TaskRunState[]{
+                    TaskRunState.COMPLETED, TaskRunState.COMPLETED,
+                    TaskRunState.COMPLETED, TaskRunState.COMPLETED,
+                    TaskRunState.COMPLETED, TaskRunState.COMPLETED
+            };
             Task[] tasksRunInOrder = run(task1).get().get();
+            pollAndAssertTaskRunState(expected, states);
             assertThat(tasksRunInOrder, is(expected));
-            Arrays.stream(expected).forEach(t -> assertThat(taskRunStateMap.get(t.id), is(TaskRunState.COMPLETED)));
+        }
 
-            // simulate running a failed task
-            taskRunStateMap.put(task1.id, TaskRunState.IN_ERROR);
+        private void testTaskFailurePropagation() throws Exception {
+            taskRunStateMap.clear();
+            makeTasks(true);
+
+            Task[] expected = new Task[]{task6, task5, task4, task3, task2, task1};
+            Task[] tasksRunInOrder = run(task1).get().get();
+            // task4 failed which means that all tasks dependent on it will be marked as failed. However it own dependencies
+            // will be marked as successful.
+            TaskRunState[] states = new TaskRunState[]{
+                    TaskRunState.COMPLETED, TaskRunState.COMPLETED,
+                    TaskRunState.IN_ERROR, TaskRunState.IN_ERROR,
+                    TaskRunState.IN_ERROR, TaskRunState.IN_ERROR
+            };
+            pollAndAssertTaskRunState(expected, states);
+            assertThat(tasksRunInOrder, is(expected));
+        }
+
+        private void testFailedTaskRerun() throws Exception {
+            taskRunStateMap.clear();
+            makeTasks(true);
+
+            Task[] expected = new Task[]{task6, task5, task4, task3, task2, task1};
+            Task[] tasksRunInOrder = run(task1).get().get();
+            // task4 failed which means that all tasks dependent on it will be marked as failed. However it own dependencies
+            // will be marked as successful.
+            TaskRunState[] states = new TaskRunState[]{
+                    TaskRunState.COMPLETED, TaskRunState.COMPLETED,
+                    TaskRunState.IN_ERROR, TaskRunState.IN_ERROR,
+                    TaskRunState.IN_ERROR, TaskRunState.IN_ERROR
+            };
+            pollAndAssertTaskRunState(expected, states);
+            assertThat(tasksRunInOrder, is(expected));
+
+            // we failed task4 so all its dependent tasks also failed. Now let's rerun the job and see that only the failed
+            // tasks run
+            makeTasks(false);
+            //only the failed tasks were run
+            Task[] expected2 = new Task[]{task4, task3, task2, task1};
+
             tasksRunInOrder = run(task1).get().get();
-
-            assertThat(tasksRunInOrder.length, is(1));
-            assertThat(tasksRunInOrder[0], is(task1));
-            Arrays.stream(expected).forEach(t -> assertThat(taskRunStateMap.get(t.id), is(TaskRunState.COMPLETED)));
-
-            taskRunner.shutdown();
+            // all tasks are now completed
+            states = new TaskRunState[]{
+                    TaskRunState.COMPLETED, TaskRunState.COMPLETED,
+                    TaskRunState.COMPLETED, TaskRunState.COMPLETED,
+                    TaskRunState.COMPLETED, TaskRunState.COMPLETED
+            };
+            pollAndAssertTaskRunState(expected, states);
+            assertThat(tasksRunInOrder, is(expected2));
         }
     }
 }
