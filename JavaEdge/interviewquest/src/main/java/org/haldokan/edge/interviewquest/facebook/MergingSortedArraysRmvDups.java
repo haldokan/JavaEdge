590cd3f1f48420fc5058d9ff8f83cf826733e82c
haytham.aldokanji <haytham.aldokanji@lab49.com>
2016-05-28 11:22:04 -0400

    My solution to a Facebook interview question

--- JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/facebook/MergingSortedArraysRmvDups.java
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/facebook/MergingSortedArraysRmvDups.java
@@ -44,22 +44,37 @@ public class MergingSortedArraysRmvDups {
                 merged[mergeIndex++] = val;
             }
         }
+
+        for (int i = secondIndex; i < second.length; i++) {
+            int val = second[i];
+            int prevMergeIndex = mergeIndex - 1;
+
+            if (val != merged[prevMergeIndex]) {
+                merged[mergeIndex++] = val;
+            }
+        }
         return Arrays.copyOf(merged, mergeIndex);
     }
 
     private void test() {
         int[] arr1 = new int[]{4, 4, 6, 8, 8, 10, 14, 14};
         int[] arr2 = new int[]{1, 1, 2, 3, 5, 5, 6, 7, 9, 11, 12, 13, 15, 15};
-
         int[] merged = merge(arr1, arr2);
         assertThat(merged, is(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}));
 
         arr1 = new int[]{4, 4, 4, 5, 5, 5, 6, 7};
         arr2 = new int[]{1, 1, 3, 3, 6, 7};
-
         merged = merge(arr1, arr2);
         assertThat(merged, is(new int[]{1, 3, 4, 5, 6, 7}));
 
-    }
+        arr1 = new int[]{1, 2, 3, 4, 4, 4};
+        arr2 = new int[]{4, 4, 4, 10, 10, 12, 12};
+        merged = merge(arr1, arr2);
+        assertThat(merged, is(new int[]{1, 2, 3, 4, 10, 12}));
 
+        arr1 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        arr2 = new int[]{9, 10, 10, 12, 12};
+        merged = merge(arr1, arr2);
+        assertThat(merged, is(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12}));
+    }
 }
