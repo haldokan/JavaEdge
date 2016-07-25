acd431c8a43a5b1b362a43d58fc89d676a39df52
haytham.aldokanji <haytham.aldokanji@lab49.com>
2016-07-25 13:40:29 -0400

    generify the implementation to accept any type extending Comparable

--- JavaEdge/search-and-sort/src/main/java/org/haldokan/edge/search/IntervalTree.java
+++ JavaEdge/search-and-sort/src/main/java/org/haldokan/edge/search/IntervalTree.java
@@ -5,75 +5,78 @@ import java.util.List;
 import java.util.Random;
 
 /**
- * My implementation of an interval tree based on descriptions of the algorithm found online
+ * My implementation of an interval tree based on descriptions of the algorithm found online - interval tree enables finding
+ * interval intersection in O(log n) where n is the number of intervals. It has space complexity of O(n)
  * The Question: 4_STAR
  * <p>
  * Created by haytham.aldokanji on 7/25/16.
  */
-public class IntervalTree {
-    private Node node;
+public class IntervalTree<T extends Comparable<T>> {
+    private Node<T> node;
 
     public static void main(String[] args) {
         test();
     }
 
     private static void test() {
-        IntervalTree driver = new IntervalTree();
+        IntervalTree<Integer> driver = new IntervalTree<>();
         Random random = new Random();
         for (int i = 0; i < 10; i++) {
-            Interval interval = new Interval(1000 + random.nextInt(1000), 2000 + random.nextInt(1000));
+            Interval<Integer> interval = new Interval<>(1000 + random.nextInt(1000), 2000 + random.nextInt(1000));
             driver.insert(interval);
         }
         driver.inOrder(driver.node);
         System.out.printf("%s%n", "----------");
         driver.preOrder(driver.node);
 
-        List<Node> overlapping = new ArrayList<>();
-        driver.findOverlapping(driver.node, new Interval(1800, 1835), overlapping);
+        List<Node<Integer>> overlapping = new ArrayList<>();
+        driver.findOverlapping(driver.node, new Interval<>(1800, 1835), overlapping);
         System.out.printf("overlapping: %s%n", overlapping);
 
         overlapping = new ArrayList<>();
-        driver.findOverlapping(driver.node, new Interval(11, 35), overlapping);
+        driver.findOverlapping(driver.node, new Interval<>(11, 35), overlapping);
         System.out.printf("overlapping: %s%n", overlapping);
     }
 
-    private void insert(Interval interval) {
+    private void insert(Interval<T> interval) {
         if (node == null) {
             interval.max = interval.end;
-            node = new Node(interval);
+            node = new Node<>(interval);
         } else {
             insert(node, interval);
         }
     }
 
-    private Node insert(Node node, Interval interval) {
+    private Node<T> insert(Node<T> node, Interval<T> interval) {
         if (node == null) {
-            return new Node(interval);
+            return new Node<>(interval);
         }
 
-        if (interval.start < node.interval.start) {
+        if (interval.start.compareTo(node.interval.start) < 0) {
             node.left = insert(node.left, interval);
         } else {
             node.right = insert(node.right, interval);
         }
-        node.interval.max = Math.max(node.interval.max, interval.max);
+        if (node.interval.max.compareTo(interval.max) < 0) {
+            node.interval.max = interval.max;
+        }
         return node;
     }
 
-    private void findOverlapping(Node node, Interval interval, List<Node> overlapping) {
+    private void findOverlapping(Node<T> node, Interval<T> interval, List<Node<T>> overlapping) {
         if (node == null) {
             return;
         }
-        if (interval.start <= node.interval.end && node.interval.start <= interval.end) {
+        if (interval.start.compareTo(node.interval.end) <= 0 && node.interval.start.compareTo(interval.end) <= 0) {
             overlapping.add(node);
         }
-        if (node.left != null && node.left.interval.max >= interval.start) {
+        if (node.left != null && node.left.interval.max.compareTo(interval.start) >= 0) {
             findOverlapping(node.left, interval, overlapping);
         }
         findOverlapping(node.right, interval, overlapping);
     }
 
-    private void inOrder(Node node) {
+    private void inOrder(Node<T> node) {
         if (node == null) {
             return;
         }
@@ -82,7 +85,7 @@ public class IntervalTree {
         inOrder(node.right);
     }
 
-    private void preOrder(Node node) {
+    private void preOrder(Node<T> node) {
         if (node == null) {
             return;
         }
@@ -91,11 +94,11 @@ public class IntervalTree {
         preOrder(node.right);
     }
 
-    private static class Interval {
-        private final int start, end;
-        private int max;
+    private static class Interval<T extends Comparable<T>> {
+        private final T start, end;
+        private T max;
 
-        public Interval(int start, int end) {
+        public Interval(T start, T end) {
             this.start = start;
             this.end = end;
             this.max = end;
@@ -107,11 +110,11 @@ public class IntervalTree {
         }
     }
 
-    private static class Node {
-        private final Interval interval;
-        private Node left, right;
+    private static class Node<T extends Comparable<T>> {
+        private final Interval<T> interval;
+        private Node<T> left, right;
 
-        public Node(Interval interval) {
+        public Node(Interval<T> interval) {
             this.interval = interval;
         }
 
