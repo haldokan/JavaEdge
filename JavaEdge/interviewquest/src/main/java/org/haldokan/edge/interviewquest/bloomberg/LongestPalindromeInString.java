9c25f3180ea1b42b06d7f0f80d026ba01c2db6aa
haytham.aldokanji <haytham.aldokanji@lab49.com>
2016-08-07 21:23:37 -0400

    My solution to a Bloomberg interview question

--- JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/bloomberg/LongestPalindromeInString.java
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/bloomberg/LongestPalindromeInString.java
@@ -1,8 +1,13 @@
 package org.haldokan.edge.interviewquest.bloomberg;
 
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
- * Some shitty interview question!
- * <p>
+ * My solution to a Bloomberg interview question - O(n^2) complexity
+ *
+ * The Question: 3_STAR
  * find the longest palindrome in a string.
  * Example:
  * string "AABCDCBA" output should be "ABCDCBA"
@@ -12,14 +17,51 @@ package org.haldokan.edge.interviewquest.bloomberg;
  */
 public class LongestPalindromeInString {
 
-    public String longestPalindrome(String str) {
-        char[] strChars = str.toCharArray();
-        char[] reverseStrChars = new char[strChars.length];
+    public static void main(String[] args) {
+        LongestPalindromeInString driver = new LongestPalindromeInString();
+        driver.testIsPalindrome();
+        driver.testLongestPalindrome();
+    }
 
-        int index = strChars.length - 1;
-        for (char chr : strChars) {
-            reverseStrChars[index--] = chr;
+    public String longestPalindrome(String str) {
+        if (str == null || str.isEmpty()) {
+            throw new IllegalArgumentException("str null or empty: " + str);
         }
-        return null;
+        if (str.length() == 1) {
+            return str;
+        }
+
+        String longestPalindrome = "";
+        for (int i = 0; i < str.length(); i++) {
+            // optimization to avoid extra work when we know that we cannot get a longer palindrome
+            if (longestPalindrome.length() > str.length() - i) {
+                break;
+            }
+            String currString = String.valueOf(str.charAt(i));
+            for (int j = i + 1; j < str.length(); j++) {
+                currString += str.charAt(j);
+                if (isPalindrome(currString) && currString.length() > longestPalindrome.length()) {
+                    longestPalindrome = currString;
+                }
+            }
+        }
+        return longestPalindrome;
+    }
+
+    private boolean isPalindrome(String str) {
+        String reverse = "";
+        for (int i = 0; i < str.length(); i++) {
+            reverse = str.charAt(i) + reverse;
+        }
+        return str.equals(reverse);
+    }
+
+    private void testIsPalindrome() {
+        assertThat(isPalindrome("racecar"), is(true));
+    }
+
+    private void testLongestPalindrome() {
+        assertThat(longestPalindrome("AABCDCBA"), is("ABCDCBA"));
+        assertThat(longestPalindrome("DEFABCBAYT"), is("ABCBA"));
     }
 }
