1fe5e2e0d3787b4cdf0ffc444aceb19e76d8a4e7
haytham.aldokanji <haytham.aldokanji@lab49.com>
2016-08-14 20:58:51 -0400

    My solution to a Bloomberg interview question

--- JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/bloomberg/MovingZerosInArrayToRight.java
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/bloomberg/MovingZerosInArrayToRight.java
@@ -2,8 +2,12 @@ package org.haldokan.edge.interviewquest.bloomberg;
 
 import java.util.Arrays;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * My solution to a Bloomberg interview question
+ *
  * The Question: 3_STAR
  * Given an unsorted integer array, place all zeros to the end of the array without changing the sequence of non-zero
  * elements. (i.e. [1,3,0,8,12, 0, 4, 0,7] --> [1,3,8,12,4,7,0,0,0])
@@ -12,10 +16,11 @@ public class MovingZerosInArrayToRight {
 
     public static void main(String[] args) {
         MovingZerosInArrayToRight driver = new MovingZerosInArrayToRight();
-        System.out.println(Arrays.toString(driver.move(new int[]{1, 3, 0, 8, 12, 0, 4, 0, 7})));
+        driver.test1();
+        driver.test2();
     }
 
-    public int[] move(int[] arr) {
+    public void move(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 0) {
                 for (int j = i; j < arr.length; j++) {
@@ -27,6 +32,34 @@ public class MovingZerosInArrayToRight {
                 }
             }
         }
-        return arr;
+    }
+
+    private void test1() {
+        int[] arr = new int[]{1, 3, 0, 8, 12, 0, 4, 0, 7};
+        move(arr);
+
+        System.out.println(Arrays.toString(arr));
+        assertThat(arr, is(new int[]{1, 3, 8, 12, 4, 7, 0, 0, 0}));
+    }
+
+    private void test2() {
+        int[] arr = new int[]{1, 3, 0, 8, 12, 0, 4, 0, 7};
+        move2(arr);
+
+        System.out.println(Arrays.toString(arr));
+        assertThat(arr, is(new int[]{1, 3, 8, 12, 4, 7, 0, 0, 0}));
+    }
+
+    // a much better version (found on-line) that runs in O(n)
+    public void move2(int[] a) {
+        int pos = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != 0) {
+                a[pos++] = a[i];
+            }
+        }
+        for (int i = pos; i < a.length; i++) {
+            a[i] = 0;
+        }
     }
 }
