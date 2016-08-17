9df4335dc6861904440d12d4ed6f3c13cbbf48c4
haytham.aldokanji <haytham.aldokanji@lab49.com>
2016-08-17 08:53:05 -0400

    My solution to an Amazon interview question

--- JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/amazon/DesignCardDeck.java
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/amazon/DesignCardDeck.java
@@ -2,9 +2,16 @@ package org.haldokan.edge.interviewquest.amazon;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
- * My solution to an Amazon interview question
+ * My solution to an Amazon interview question - I used an array with a current index that points to the top of the deck
+ * and moves up and down as the cards are stacked and unstacked. We can shuffle the deck at any point taking into account
+ * only the available cards. We can get the available cards and reset the deck to make all cards available (basically
+ * starting a new round).
  * <p>
  * The Question: 3_STAR + 1/2
  * <p>
@@ -14,8 +21,41 @@ import java.util.Random;
  * <p>
  * Created by haytham.aldokanji on 8/16/16.
  */
-// todo add tests
 public class DesignCardDeck {
+
+    public static void main(String[] args) {
+        DesignCardDeck driver = new DesignCardDeck();
+        driver.test();
+    }
+
+    private void test() {
+        int deckSize = 52;
+        Card[] cards = new Card[deckSize];
+        IntStream.range(0, deckSize).forEach(i -> cards[i] = new Card("card" + i));
+
+        Deck deck = new Deck(cards);
+        System.out.printf("%s%n", deck);
+
+        deck.shuffle();
+        System.out.printf("%s%n", deck);
+
+        Card[] dealtCards = deck.deal(7);
+        assertThat(dealtCards.length, is(7));
+
+        Card[] availableCards = deck.getAvailableCards();
+        System.out.printf("Available: %s%n", Arrays.toString(availableCards));
+        assertThat(availableCards.length, is(deckSize - 7));
+
+        deck.stackBack(3);
+        availableCards = deck.getAvailableCards();
+        System.out.printf("Available: %s%n", Arrays.toString(availableCards));
+        assertThat(availableCards.length, is(deckSize - 4));
+
+        deck.reset();
+        availableCards = deck.getAvailableCards();
+        System.out.printf("Available: %s%n", Arrays.toString(availableCards));
+        assertThat(availableCards.length, is(deckSize));
+    }
 
     private static class Deck {
         private final Card[] cards;
@@ -27,7 +67,7 @@ public class DesignCardDeck {
                 throw new IllegalArgumentException("Some cards are duplicate: " + Arrays.toString(cards));
             }
             this.cards = Arrays.copyOf(cards, cards.length);
-            this.topCardIndex = cards.length;
+            this.topCardIndex = cards.length - 1;
         }
 
         public void shuffle() {
@@ -58,6 +98,22 @@ public class DesignCardDeck {
                 throw new IllegalArgumentException("Too many cards to stack back: " + numCards);
             }
             topCardIndex += numCards;
+        }
+
+        public Card[] getAvailableCards() {
+            return Arrays.copyOf(cards, topCardIndex + 1);
+        }
+
+        public void reset() {
+            topCardIndex = cards.length - 1;
+        }
+
+        @Override
+        public String toString() {
+            return "Deck{" +
+                    "topCardIndex=" + topCardIndex +
+                    ", cards=" + Arrays.toString(cards) +
+                    '}';
         }
     }
 
