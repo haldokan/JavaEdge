b9d3afe8060c228fe5b0ca8cb1f1a126c8c9ba71
haytham.aldokanji <haldokan@gmail.com>
2016-09-10 20:40:54 -0400

    My implementation of consisten hashing

--- JavaEdge/how-things-work/src/main/java/org/haldokan/edge/ConsistentHashing.java
+++ JavaEdge/how-things-work/src/main/java/org/haldokan/edge/ConsistentHashing.java
@@ -9,22 +9,29 @@ import java.nio.charset.Charset;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
- * My implementation of Consistent Hashing with support form removing and adding hash buckets (we use a cluster
- * of machines to represent buckets).
+ * My implementation of Consistent Hashing with support for removing and adding hash buckets (I use a cluster
+ * of machines arranged in a ring for the concrete implementation).
+ *
  * todo: add machine aliases to mitigate the case where 2 machines are too close to each other in the hash ring
  *
  * The Question: 5_STAR
+ *
+ * From Wikipedia: Consistent hashing is a special kind of hashing such that when a hash table is resized, only
+ * K/n keys need to be remapped on average, where K is the number of keys, and n is the number of slots. In contrast,
+ * in most traditional hash tables, a change in the number of array slots causes nearly all keys to be remapped because
+ * the mapping between the keys and the slots is defined by a modular operation.
  *
  * Created by haytham.aldokanji on 8/16/16.
  */
 public class ConsistentHashing {
     private final NavigableSet<Double> hashRing;
     private final double HASH_SLOT_LEN = 1d / Integer.MAX_VALUE;
-    HashFunction hashFunction = Hashing.md5();
+    private final HashFunction hashFunction;
     // we could have used 2 maps but Guava's BiMap is a more concise way to do it
     private BiMap<Double, Machine> machineByPosition;
     private Map<String, Machine> machineById = new HashMap<>();
@@ -45,63 +52,79 @@ public class ConsistentHashing {
     }
 
     public void addMachines(String... machineIds) {
+        System.out.printf("Add machines: %s%n", Arrays.toString(machineIds));
+
         for (String machineId : machineIds) {
             double position = Math.random();
             hashRing.add(position);
-            System.out.printf("hashRing: %s%n", hashRing);
 
-            machineByPosition.put(position, machineById.get(machineId));
+            Machine machine = machineById.get(machineId);
+            machineByPosition.put(position, machine);
+
             if (hashRing.size() > 1) {
-                rehashDataRange(position);
+                redistributeDataOnAddingMachine(machine, position);
             }
         }
     }
 
     public void removeMachines(String... machineIds) {
+        System.out.printf("Remove machines: %s%n", Arrays.toString(machineIds));
+
         // we are shutting down the cluster - in real app we should verify that machine ids actually exist in the cluster
         if (machineIds.length >= hashRing.size()) {
             hashRing.clear();
             machineByPosition.clear();
         } else {
             // here Guava BiMap comes to play
+            List<Machine> removedMachines = new ArrayList<>();
+            List<Double> removedPositions = new ArrayList<>();
+
+            // remove all machines before redistributing the data
             for (String machineId : machineIds) {
                 Machine machine = machineById.get(machineId);
                 double machinePosition = machineByPosition.inverse().get(machine);
                 hashRing.remove(machinePosition);
                 machineByPosition.inverse().remove(machine);
-                //todo find next machine and unload data to it: similar to rehashDataRange (can we refactor)?
+
+                removedMachines.add(machine);
+                removedPositions.add(machinePosition);
+            }
+
+            Iterator<Machine> machineIterator = removedMachines.iterator();
+            for (double removedPosition : removedPositions) {
+                redistributeDataOnRemovingMachine(machineIterator.next(), removedPosition);
             }
         }
     }
 
     public void processDataItem(DataItem dataItem) {
-        Machine machine = findMachine(dataItem.getKey());
+        int hashCode = hashFunction.hashString(dataItem.getKey(), Charset.defaultCharset()).asInt();
+        double hashPosition = Math.abs(hashCode * HASH_SLOT_LEN);
+
+        Machine machine = findMachine(hashPosition, hashRing::floor);
         machine.processDataItem(dataItem);
     }
 
-    private void rehashDataRange(Double newMachinePosition) {
-        Machine newMachine = machineByPosition.get(newMachinePosition);
-        Machine potentialNextMachine = machineByPosition.get(hashRing.lower(newMachinePosition));
-        Machine nextMachine = potentialNextMachine != null ? potentialNextMachine : machineByPosition.get(hashRing.last());
-
+    private void redistributeDataOnAddingMachine(Machine machine, Double machinePosition) {
+        Machine nextMachine = findMachine(machinePosition, hashRing::lower);
 
         Set<DataItem> dataItemsForRedistribution = nextMachine.getDataItemsForRedistribution(dataItem -> {
             double dataItemHash = Math.abs(hashFunction.hashString(dataItem,
                     Charset.defaultCharset()).asInt() * HASH_SLOT_LEN);
-            return dataItemHash > newMachinePosition;
+            return dataItemHash > machinePosition;
         });
 
-        dataItemsForRedistribution.forEach(newMachine::processDataItem);
+        dataItemsForRedistribution.forEach(machine::processDataItem);
     }
 
-    private Machine findMachine(String data) {
-        int hashCode = hashFunction.hashString(data, Charset.defaultCharset()).asInt();
-        double hashPosition = Math.abs(hashCode * HASH_SLOT_LEN);
-        // counter clockwise find the nearest machine
-        System.out.printf("%f%n", hashPosition);
+    private void redistributeDataOnRemovingMachine(Machine machine, Double machinePosition) {
+        Set<DataItem> dataItemsForRedistribution = machine.getDataItemsForRedistribution(dataItem -> true);
+        Machine nextMachine = findMachine(machinePosition, hashRing::lower);
+        dataItemsForRedistribution.forEach(nextMachine::processDataItem);
+    }
 
-        // if position is less than the position of the 1st machine then no machine will be found
-        Double potentialPosition = hashRing.floor(hashPosition);
+    private Machine findMachine(double hashPosition, Function<Double, Double> positionFunc) {
+        Double potentialPosition = positionFunc.apply(hashPosition);
         // circle from start to end of the set taking the machine at the highest position
         double machinePosition = potentialPosition != null ? potentialPosition : hashRing.last();
         return machineByPosition.get(machinePosition);
@@ -132,10 +155,19 @@ public class ConsistentHashing {
 
                 addMachines(extraMachine1.getId(), extraMachine2.getId());
             }
+
+            if (i == 6000) {
+                removeMachines("MID3000");
+            }
+
             if (i == 8000) {
                 Machine extraMachine3 = new Machine("MID" + i);
                 machineById.put(extraMachine3.getId(), extraMachine3);
                 addMachines(extraMachine3.getId());
+            }
+
+            if (i == 9500) {
+                removeMachines("MID8000");
             }
         }
     }
@@ -159,13 +191,11 @@ public class ConsistentHashing {
         }
 
         public Set<DataItem> getDataItemsForRedistribution(Predicate<String> hashPredicate) {
-            Set<String> keys = data.keySet().stream().filter(hashPredicate::test).collect(Collectors.toSet());
-            Set<DataItem> dataItems = new HashSet<>();
+            Set<String> keys = data.keySet().stream()
+                    .filter(hashPredicate::test)
+                    .collect(Collectors.toSet());
 
-            for (String key : keys) {
-                dataItems.add(data.remove(key));
-            }
-            return dataItems;
+            return keys.stream().map(data::remove).collect(Collectors.toSet());
         }
 
         @Override
@@ -189,14 +219,11 @@ public class ConsistentHashing {
             return key;
         }
 
-        public byte[] getPayload() {
-            return payload;
-        }
-
         @Override
         public String toString() {
             return "DataItem{" +
                     "key='" + key + '\'' +
+                    ", payload=" + Arrays.toString(payload) +
                     '}';
         }
     }
