fe9c236610691f98fae116e3d93cf83a120748f2
haldokanji <haldokanji@bloomberg.net>
2020-07-12 21:39:11 -0400

    new Amazon interview questions

---
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/amazon/ArrayUnionAndIntersection.java
@@ -0,0 +1,10 @@
+package org.haldokan.edge.interviewquest.amazon;
+
+/**
+ * Implement union and intersection of two array(in a efficient way).
+ *
+ * Additional information given by the interviewer was: elements of the two given array may be repeated but cannot be
+ * repeated in union and intersection array.
+ */
+public class ArrayUnionAndIntersection {
+}
---
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/amazon/BloodRelationBasedOnIdCards.java
@@ -0,0 +1,19 @@
+package org.haldokan.edge.interviewquest.amazon;
+
+/**
+ * Given billions of Identity cards of the form :
+ *
+ *
+ * card : {
+ *    my_id : "my id",
+ *    "moms_id" : "mom id",
+ *    "dad_id" : "dads id"
+ * }
+ * If one gives you two Person's id, how can you tell if these 2 persons are blood related.
+ * So, write a function that is:
+ *
+ *
+ * def is_blood_related( person_id_1, person_id_2 )
+ */
+public class BloodRelationBasedOnIdCards {
+}
