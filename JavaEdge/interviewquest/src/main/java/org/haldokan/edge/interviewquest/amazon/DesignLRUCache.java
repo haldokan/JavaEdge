8fa9c966fd842f04a49d1c2f0bc951081c2c676a
haldokanji <haldokanji@bloomberg.net>
2020-07-14 15:44:42 -0400

    add more cache api

--- JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/amazon/DesignLRUCache.java
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/amazon/DesignLRUCache.java
@@ -2,6 +2,7 @@ package org.haldokan.edge.interviewquest.amazon;
 
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.Objects;
 import java.util.PriorityQueue;
 import java.util.concurrent.*;
 
@@ -14,21 +15,46 @@ public class DesignLRUCache {
     PriorityQueue<Key> lru = new PriorityQueue<>((k1, k2) -> k1.time.isBefore(k2.time) ? 1 : -1); // min-heap on time
 
     int maxSize;
-    int parallelLevel;
-
+    BlockingQueue<Key>[] stagingQueues;
 
     public DesignLRUCache(int maxSize, int parallelLevel) {
         this.maxSize = maxSize;
-        this.parallelLevel = parallelLevel;
+        this.stagingQueues = new ArrayBlockingQueue[parallelLevel]; // example of where Java sucks since array are always different (no easy way to use generics here)
+
     }
 
-    byte[] put(String key, byte[] val) {
-        byte[] prevVal = cache.put(key, val);
-        lru.add(new Key(key)); // can be made to be deposited first on a number of blocking queues to lessen contention and then added to the lru min-heap.
+    byte[] put(String id, byte[] val) {
+        byte[] prevVal = cache.put(id, val);
+
+        Key key = new Key(id);
+        stagingQueues[id.hashCode() % stagingQueues.length].offer(key); // reduce contention by usign a number of blocking queues
         return prevVal;
     }
 
-    // runs periodically on a lower priority thread
+    // uncommon operation in caches generally
+    void remove(String id) {
+        cache.remove(id);
+        // alternatively we can let the key age in the lru heap so and removed later by the cache maintainer thread
+        lru.remove(new Key(id)); // note that Key equality is done on the id only so it can be found in the lru heap (this is O(n) operation).
+    }
+
+    // move keys from blocking queues to lru min-heap
+    void addToLru() {
+        for (; ; ) {
+            for (BlockingQueue<Key> queue : stagingQueues) {
+                try {
+                    Key key = queue.poll();
+                    if (key != null) {
+                        lru.add(key);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace(); // we should handle exceptions differently
+                }
+            }
+        }
+    }
+
+    // runs periodically on a (lower priority) thread and remove the least recently accessed entries from the cache
     void maintainCache() {
         ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
         service.schedule(() -> {
@@ -46,6 +72,19 @@ public class DesignLRUCache {
         public Key(String id) {
             this.id = id;
             this.time = LocalDateTime.now();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Key key = (Key) o;
+            return Objects.equals(id, key.id);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id);
         }
     }
 }
