756828ffec99783f067ef0282ef9ea1c4fd97cf5
haldokan <haldokan@gmail.com>
2020-10-25 20:05:45 -0400

    commit class in-the-works in order to merge upstream

---
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/google/MovesSequenceForWordEntryInLetterMatrix.java
@@ -0,0 +1,20 @@
+package org.haldokan.edge.interviewquest.google;
+
+/**
+ * https://www.careercup.com/question?id=15542726
+ * Given the English alphabet, 'a' through 'z' (lowercase), and an imaginary onscreen keyboard with the letters laid out
+ * in 6 rows and 5 columns:
+ *
+ *
+ * a b c d e
+ * f g h i j
+ * k l m n o
+ * p q r s t
+ * u v w x y
+ * z
+ * Using a remote control - (up - 'u', down 'd', left 'l', right 'r' and enter '!'), write a function that given a word
+ * will produce the sequence of key presses required to type out the word on the onscreen keyboard. The function should
+ * return the sequence string.
+ */
+public class MovesSequenceForWordEntryInLetterMatrix {
+}
