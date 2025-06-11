00811f04710f427fa15404358ccf0f9372a05084
haldokanji <haldokanji@bloomberg.net>
2025-06-10 23:12:43 -0400

    lrs

--- JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/amazon/PostfixExpressions.java
+++ JavaEdge/interviewquest/src/main/java/org/haldokan/edge/interviewquest/amazon/PostfixExpressions.java
@@ -7,46 +7,36 @@ import java.util.Deque;
  * My solution to an Amazon interview question
  * The Question: 3_STAR
  * Given a postfix expression as a string, evaluate it and return the result.
- * Example : "423+*" ->20. The Postfix expression is well formed(need not check for bad expression)
+ * Example : "423+*" ->20. The Postfix expression is well-formed (need not check for bad expression)
+ * Touched on 06/08/25 (partially suggested by AI)
  */
 public class PostfixExpressions {
-    private static final String M = "*", A = "+", S = "-", D = "/";
-
     public static void main(String[] args) {
-        System.out.println(eval("32423+*/-")); //should be 7: (2 + 3) * 4 / 2 - 3
-        System.out.println(eval("423+*")); //should be 20
+        System.out.println(eval("32423+*/-")); // Should be 7: (2 + 3) * 4 / 2 - 3
+        System.out.println(eval("423+*")); // Should be 20
+        System.out.println(eval("595*+")); // Should be 9 * 5 + 5 = 50
     }
 
-    private static Double eval(String expr) {
-        Deque<String> stack = new ArrayDeque<>();
-        for (int i = 0; i < expr.length(); i++) {
-            String chr = String.valueOf(expr.charAt(i));
-            if (isOperator(chr)) {
-                stack.push(apply(stack.pop(), stack.pop(), chr));
+    private static int eval(String expr) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (char chr : expr.toCharArray()) {
+            int num = chr - '0'; // Convert char to int
+            if (Character.isDigit(chr)) {
+                stack.push(num);
             } else {
-                stack.push(chr);
+                stack.push(apply(stack.pop(), stack.pop(), chr));
             }
         }
-        return Double.valueOf(stack.pop());
+        return stack.pop();
     }
 
-    private static String apply(String chr1, String chr2, String operator) {
-        if (operator.equals(M)) {
-            return String.valueOf(Double.valueOf(chr1) * Double.valueOf(chr2));
-        }
-        if (operator.equals(A)) {
-            return String.valueOf(Double.valueOf(chr1) + Double.valueOf(chr2));
-        }
-        if (operator.equals(S)) {
-            return String.valueOf(Double.valueOf(chr1) - Double.valueOf(chr2));
-        }
-        if (operator.equals(D)) {
-            return String.valueOf(Double.valueOf(chr1) / Double.valueOf(chr2));
-        }
-        throw new IllegalArgumentException("Invalid operator: " + operator);
-    }
-
-    private static boolean isOperator(String chr) {
-        return chr != null && (chr.equals(M) || chr.equals(A) || chr.equals(S) || chr.equals(D));
+    private static int apply(int num1, int num2, int operator) {
+        return switch (operator) {
+            case '+' -> num1 + num2;
+            case '-' -> num1 - num2;
+            case '*' -> num1 * num2;
+            case '/' -> num1 / num2;
+            default -> throw new IllegalArgumentException("Invalid operator: " + operator);
+        };
     }
 }
